343581b3ac123d26ba3623d55b6d268103bd149f
chenyl <18301662790@163.com>
2016-07-14 17:34:44 +0800

    Some helpful message for your future self

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   #resources :topics
   resources :topics do
     member do
-      post 'upvote'
+      post 'upvote','downvote'
     end
+    
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
