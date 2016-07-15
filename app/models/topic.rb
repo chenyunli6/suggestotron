0707e6841f74e320ed5ddc0d4a447542db6b4263
chenyl <18301662790@163.com>
2016-07-15 13:36:12 +0800

    orderTopics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ApplicationRecord
   has_many :votes, dependent: :destroy
+  scope :orderTopics,-> {select("topics.*, count(votes.id) AS votes_count").
+  joins(:votes).
+  group("topics.id").
+  order("votes_count DESC")}
 end
