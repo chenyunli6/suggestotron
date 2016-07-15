81ba3b99c23954cdc028d13af48c752797475e54
chenyl <18301662790@163.com>
2016-07-15 19:37:25 +0800

    creare about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
   root 'topics#index'
   #resources :topics
   resources :topics do
     member do
       post 'upvote','downvote'
     end
-
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
